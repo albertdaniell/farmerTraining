cbedfe22241580d53b2b125aa28a6faf5547da5b
albertdaniell <albertagoya@gmail.com>
2021-09-15 07:20:31 +0300

    hehe

---
+++ build/precache-manifest.6a7760cdc4c149740fcfd153b56c3c66.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849baeafc62a1e205a975a40b8c65fff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5fc51e75a6399183875",
+    "url": "/static/css/main.8636fd5a.chunk.css"
+  },
+  {
+    "revision": "63f7b85c84c587ac7bb2",
+    "url": "/static/js/2.a51c06da.chunk.js"
+  },
+  {
+    "revision": "e2cf1b41d5ca280a94b140a3bfcdc050",
+    "url": "/static/js/2.a51c06da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5fc51e75a6399183875",
+    "url": "/static/js/main.e4da3038.chunk.js"
+  },
+  {
+    "revision": "c4cf953c068e1ea36396",
+    "url": "/static/js/runtime-main.37d012ff.js"
+  },
+  {
+    "revision": "90590ad714e20f500e1e320dc3a8b822",
+    "url": "/static/media/cow.90590ad7.jpg"
+  },
+  {
+    "revision": "b419c6a978f68c7c93dfc2de78b24a22",
+    "url": "/static/media/greengrams.b419c6a9.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "60a2d383be51c6d77a8c26c06bb19686",
+    "url": "/static/media/sorghum-image.60a2d383.jpg"
+  },
+  {
+    "revision": "d8c12d1e66db81c03b0da1e7ec33a0c8",
+    "url": "/static/media/sorghum.d8c12d1e.jpg"
+  }
+]);
